0a73b55c8db4d6dded5a398375ca487e51822517
Teletrup <saletrapotasowatosyf@gmail.com>
2022-07-31 00:35:46 +0200

    min-dyn kreck start

---
+++ kreck/lisp/min-dyn/kreck.lisp
@@ -0,0 +1,162 @@
+(defparameter dbg nil)
+
+(defmacro fn (args &body body) 
+  `(lambda ,args ,@body))
+
+(defun thread-sub (a b) 
+  (subst a '$$ b))
+
+(defmacro $$-> (input &body exprs) 
+  (reduce #'thread-sub (cons input exprs)))
+
+
+(defvar *test-macros* '(test test-equal))
+
+;macrolet & stuff?
+(defun test-1 (expr) 
+  `(if ,expr 
+       t 
+       (progn ,(unless (member (car expr) *test-macros*)
+                   `(format t "failed: ~s~%" (quote ,expr))) 
+              nil)))
+
+(defmacro test (name &body exprs) 
+  `(and ,@(mapcar #'test-1 exprs)))
+
+(defmacro test-equal (name &body exprs)
+  `(test ,name
+     ,@(mapcar (fn (e)
+                 (cons 'equal e)) 
+               exprs)))
+
+
+
+;kreck
+
+(defun kreck-apply (subj op args)
+  (if (atom op)
+      (funcall op subj args)
+      (let ((clos (kreck-eval subj op))) 
+        (kreck-eval (acons "args" 
+                           args
+                           ;(kreck-evlis subj args)
+                           (cdr clos))
+                    (car clos)))))
+
+(defun kreck-eval (subj form)
+  (if (atom form)
+      form
+      (kreck-apply subj
+                   (kreck-eval subj (car form))
+                   (cdr form))))
+
+
+;exops
+
+(defun subj-op (subj args) 
+  subj)
+
+(defun quot-op (subj args) 
+  (car args))
+
+(defun car-op (subj args)
+  (car (kreck-eval subj (car args))))
+
+(defun cdr-op (subj args)
+  (cdr (kreck-eval subj (car args))))
+
+(defun cons-op (subj args)
+  (cons (kreck-eval subj (car args))
+        (kreck-eval subj (cadr args))))
+
+(defun eval-op (subj args)
+  (kreck-eval (kreck-eval subj (car args))
+              (kreck-eval subj (cadr args))))
+
+(defun if-op (subj args) 
+  (if (kreck-eval subj (car args))
+      (kreck-eval subj (cadr args))  
+      (kreck-eval subj (caddr args))))
+
+
+
+(defun assoc-op (subj args)
+  (assoc (kreck-eval subj (car args))
+         (kreck-eval subj (cadr args))
+         :test #'equal))
+
+(defun getvar-op (subj args)
+  (let* ((key (kreck-eval subj (car args)))
+         (res (assoc key subj :test #'equal)))
+    (if res
+        (cdr res)
+        (error (format nil "var ~a not found~%" key)))))
+
+
+;defs
+
+
+
+(defun parse (expr)
+  (cond ((null expr) nil)
+        ((symbolp expr) (list #'getvar-op
+                              (string-downcase expr)))
+        ((consp expr) (cons (parse (car expr))
+                            (parse (cdr expr))))
+        (t expr)))
+
+(defun unparse (expr)
+  (cond ((atom expr) expr)
+        ((and (eq (car expr) #'getvar-op)
+              (stringp (cadr expr)))
+         (let ((s (read-from-string (cadr expr)))) 
+           s))
+        (t (cons (unparse (car expr))
+                 (unparse (cdr expr))))))
+
+(defun def-make (name &rest expr)
+  (cons name (parse (car expr))))
+
+(defun kreck (args form)
+  (kreck-eval (acons "args" (parse args) *defs*)
+              (parse form)))
+
+(defparameter *defs*
+  `(("~" . nil)
+    ("$" . ,#'subj-op)
+    ("q" . ,#'quot-op)  
+    ("<" . ,#'car-op)  
+    (">" . ,#'cdr-op)  
+    ("c" . ,#'cons-op)  
+    ("*" . ,#'eval-op)
+    ("?" . ,#'if-op)
+    ("#" . ,#'getvar-op)
+    ("l" . ,(parse '(c (q (? args
+                             (c (* (> ($))
+                                   (< args))
+                                (* (> ($))
+                                   (c (q l)
+                                      (> args))))
+                             ~))
+                       ($))))
+    ("gate" . ,(parse '(c (q (l (q c)
+                                (l (q q) (< args))
+                                (l (q c)
+                                   (l (q c)
+                                      "rec"
+                                      (l (q q) (< args)))
+                                   (< (> args)))))
+                          ($))))
+    ))
+;^ with acons
+;(c (q (? (* (> ($)) args)) (q 1) (q 0))
+;                       ($)  would seggie in c
+
+;name-based recursion and problem of cycles after substitution
+
+
+
+
+
+(kreck '(1 2 3) '(l (> args) 1))
+(unparse (kreck '(1) '(gate (c args args) ($))))
